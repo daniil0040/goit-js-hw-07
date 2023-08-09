dcf6dc24ee4ef5b2ec2a922df522c6d358d6919b
daniil0040 <danyapolishchuk777@gmail.com>
2023-08-09 02:36:31 -0500

    hw 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,24 +2,14 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const container = document.querySelector(".gallery");
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems))
 function createMarkup(arr) {
-    const markup = arr.map(({ preview, original, description }) => `<li class='gallery__item js-gallery__item'>
+    return arr.map(({ preview, original, description }) => `<li class='gallery__item'>
     <a class='gallery__link' href ="${original}"> 
-    <img class="gallery__image"
+    <img class="gallery__image js-gallery__image"
     src="${preview}"
     alt="${description}"/>
     </a>
-    </li>`).join("")  
-    container.insertAdjacentHTML("beforeend", markup)
+    </li>`).join("")
 }
-createMarkup(galleryItems)
-
-var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
-console.log(lightbox);
-// {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-//     description: 'Hokkaido Flower',
-//   }
+ new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay : 250});
